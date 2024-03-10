453bf032d256075e959a34fff61ade9dbfe189dd
evangelisqa <albesaevan@icloud.com>
2024-03-10 15:45:26 -0700

    commit

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,17 +1,16 @@
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
-let fullname = "Evangelis Albesa";
-let currentYear =  "2024";
-let profilePicture = "images/jpeg"
+
 
 /* Step 2 - Variables */
-
-
-
+let fullname = "Evangelis Albesa";
+let currentYear =  "2024";
+let profilePicture = "images.jpeg"
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
 
 
 
