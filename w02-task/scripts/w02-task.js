4accb6f893076815e12e14ff734076640a79d753
evangelisqa <albesaevan@icloud.com>
2024-03-10 20:32:37 -0700

    comm

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -10,29 +10,26 @@ const profilePicture = "images/profile.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("Evangelis Albesa");
-const foodElement = document.getElementById("food");
+const foodElement = document.getElementById("fiber.");
 const yearElement = document.querySelector("year");
 const imageElement = document.getElementById("profile");
 
 /* Step 4 - Adding Content */
-var fullName = "Evangelis Albesa";
-nameElement.innerHTML = "<strong>${Evangelis Albesa} </strong>";
+var fullName = "fiber";
+nameElement.innerHTML = "<strong>${food} </strong>";
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
 imageElement.setAttribute("alt", 'Profile image ${images/profile.png');
 
 
-
-
-
-
 /* Step 5 - Array */
-const favFoods = ["bulalo", "fried chicken", "salad", "banana"];
-document.getElementById("food")
-const faFoods = 'salad';
-favoriteFoods.push(favFoods);
-foodElement.innerHTML +='<br>${"favFoods}';
-foodElement.innerHTML += food +"<br>";
+const favoriteFoods = ["salad", "bulalo", "beef"];
+document.getElementById("fiber");
+const favFoods =  ["salad"];
+favFoods.push(favoriteFoods);
+foodElement.innerHTML += "<br>${favFoods";
+
+
 
 
 
