8f240d75d07c3069336ab249466a6199ed84fa8d
evangelisqa <albesaevan@icloud.com>
2024-03-10 19:39:44 -0700

    comm

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,21 +4,27 @@
 
 
 /* Step 2 - Variables */
-let fullname = "Evangelis Albesa";
-let currentYear =  "2024";
-let profilePicture = "images.jpeg"
+const fullname = "Evangelis Albesa";
+const currentYear =  new Date().getFullYear();
+const profilePicture = "images/profile.png";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("name");
+const nameElement = document.getElementById("Evangelis Albesa");
 const foodElement = document.getElementById("food");
-const yearElement = document.querySelector("#year");
-const imageElement = document.getElementById("prof");
+const yearElement = document.querySelector("year");
+const imageElement = document.getElementById("profile");
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = "<strong>${fullName}</strong>";
-nameElement.textContent = currentYear;
-imageElement.setAttribute("scr", profilePicture);
-imageElement.setAttribute("alt", "Profile image of $ {NameVariable}");
+var fullName = "Evangelis Albesa";
+nameElement.innerHTML = "<strong>${Evangelis Albesa} </strong>";
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", 'Profile image ${images/profile.png');
+
+
+
+
+
 
 /* Step 5 - Array */
 const favFoods = ["bulalo", "fried chicken", "salad", "banana"];
