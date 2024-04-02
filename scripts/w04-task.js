4575853cacbaa662c10358589420fb6e20d14715
evangelisqa <albesaevan@icloud.com>
2024-04-01 22:39:38 -0700

    co

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,78 +1,61 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
+/* Profile Object  */
 let myProfile = {
     name: 'Evangelis Albesa',
     photo: 'images/prof.png',
-    favoriteFoods:  ['Bulalo',
-    'Sinigang',
-      'Salad'],
-    hobbies: [
-    'Exploring', 
-    'Hiking',
-    'Reading', 
-    'Photography'],
-
-    placesLived:(
-        {
-            place: "British Columbia",
-            length: "2 years"
-        },
-        {   
-            place: "Regina",
-            length: "8 years"
-        },
-        {
-            place: "Vancouver",
-            length: "2 years"
-        }
-    )
+    favoriteFoods: ['Bulalo', 'Adobo chicken', 'Chicken tinola', 'Pasta'],
+    hobbies: ['Travelling', 'Web Design', 'Movies', 'Studying Scriptures', 'Drawing'],
+    placesLived: []
 };
-
-
 
 /* Populate Profile Object with placesLive objects */
 
+myProfile.placesLived.push({
+    place: 'Alberta',
+    length: '2 Years'
+});
+myProfile.placesLived.push({
+    place: 'Saskatchewan',
+    length: '8 Years'
+});
+myProfile.placesLived.push({
+    place: 'British Columbia',
+    length: '2 Years'
+})
 
 
 /* DOM Manipulation - Output */
 
-
 /* Name */
-document.querySelector("#name").textContent = myProfile.name;
+document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
-document.querySelector("#photo").src = myProfile.photo;
-document.querySelector("#hoto").alt = myProfile.name;
+
+document.querySelector('#photo').setAttribute('alt', myProfile.name);
+
 
 /* Favorite Foods List*/
-document.querySelector("f#avorite-foods").textContent = myProfile;
 myProfile.favoriteFoods.forEach(food => {
-    let listItem = document.createElement("li");
-    listItem.textContent = food;
-    favoriteFoodsList.appendChild(listItem);
-   
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
 });
-
 
 /* Hobbies List */
-myProfile.hobbies.forEach(hobby =>
-{
-    let li = document.createElement("li");
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
     li.textContent = hobby;
-    document.querySelector("#hobbieslist").appendChild(li);
+    document.querySelector('#hobbies').appendChild(li);
 });
 
-
 /* Places Lived DataList */
-myProfile.placesLived.forEach(place =>{
-    let dt = document.createElement("dt");
-    dt.textContent = place.place;
-
-    let dd = document.createElement("dd");
-    dd.textContent.Content = place.length;
-
-    let dl = document.querySelector("#places-lived");
-    dl.appendChild(dt);
-    dl.appendChild(dd);
-})
+myProfile.placesLived.forEach(placeLived => {
+    let dt = document.createElement('dt');
+    dt.textContent = placeLived.place;
+    document.querySelector('#places-lived').appendChild(dt);
+    let dd = document.createElement('dd');
+    dd.textContent = placeLived.length;
+    document.querySelector('#places-lived').appendChild(dd);
+});
