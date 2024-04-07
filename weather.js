502e2eb8a2ac106a9baa183c67d97097c466499e
evangelisqa <albesaevan@icloud.com>
2024-04-06 23:44:13 -0700

    commit

--- weather.js
+++ weather.js
@@ -1,5 +1,3 @@
-
-
 async function getWeather(){
     const location = document.getElementById('locationInput').Value;
     const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
