45efe6dae7b1c56215c041a34050d368c113e60c
evangelisqa <albesaevan@icloud.com>
2024-04-10 00:58:43 -0700

    commit

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -104,7 +104,6 @@ document.getElementById('getTotal').addEventListener('click', function() {
 });
 
 
-
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
