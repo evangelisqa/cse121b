e6db9c7cb9b2cfbbec07f0f4a2cf16e5be04b8c4
evangelisqa <albesaevan@icloud.com>
2024-04-19 21:01:12 -0700

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,35 @@
-// copyright year
-document.querySelector('#year').textContent = new Date().getFullYear();
+const myImage = document.querySelector("img");
 
-// toggle menu in small view
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
-
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
-
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
